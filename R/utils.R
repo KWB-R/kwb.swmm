ba9cec33ebede0931a00f5fded9dd21d29e4788f
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-04-14 16:06:31 +0200

    add conversion function

--- R/utils.R
+++ R/utils.R
@@ -7,3 +7,19 @@
 extdata_file <- function(...) {
   system.file("extdata", ..., package = "kwb.swmm")
 }
+
+
+#' Convert squaremeter to hectar
+#'
+#' @param squaremeter squaremeter
+#'
+#' @return returns hectars
+#' @export
+#'
+#' @examples
+#' squaremeter_to_hectar(10000)
+#'
+squaremeter_to_hectar <- function(squaremeter) {
+  squaremeter / 10000
+}
+
